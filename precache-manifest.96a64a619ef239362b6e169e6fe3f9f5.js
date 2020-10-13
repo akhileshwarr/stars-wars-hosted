4097ffc89143f516213cd1e0fb17f0a242c10d33
AK351822 <akhileshwar-rachakon>
2020-10-13 21:19:24 +0530

    init commit

---
+++ precache-manifest.96a64a619ef239362b6e169e6fe3f9f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86ecb114b30cb3ab0c42131c89d1b06c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f301563665d7f932bf6b",
+    "url": "/static/css/main.0a26df67.chunk.css"
+  },
+  {
+    "revision": "e3363ab0a86c56e48b76",
+    "url": "/static/js/2.7cf351c1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.7cf351c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f301563665d7f932bf6b",
+    "url": "/static/js/main.14f9a4de.chunk.js"
+  },
+  {
+    "revision": "cb91f0edb559e92ada85",
+    "url": "/static/js/runtime-main.c2a2f9df.js"
+  }
+]);
